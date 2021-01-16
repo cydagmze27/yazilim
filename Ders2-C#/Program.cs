4bfb3ca3cfc3cfc48d93d08f4cce552269c70586
CEYDA GAMZE TOSUN <58703800+cydagmze27@users.noreply.github.com>
2021-01-17 02:12:05 +0300

    Add files via upload

---
+++ Ders2-C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu")
+            }
+        }
+    }
+}
