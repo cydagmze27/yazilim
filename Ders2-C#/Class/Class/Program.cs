62dd066355e15d2fa1c4ae38ddb0b986de322ca2
CEYDA GAMZE TOSUN <58703800+cydagmze27@users.noreply.github.com>
2021-01-17 02:16:02 +0300

    Add files via upload

---
+++ Ders2-C#/Class/Class/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "CGT";
+            int age = 27;
+
+            Product product1 = new Product(); //Sınıfımızdaki 1.inci ürünümüzü ve özelliklerini belirlerdik.
+            product1.ProductName = "Jacket";
+            product1.ProductBrand = "Pull&Bear";
+            product1.ProductModel = "Leather Jacket";
+            product1.ProductSize = 36;
+
+            Product product2 = new Product(); //Sınıfımızdaki 2.inci ürünümüzü ve özelliklerini belirlerdik.
+            product2.ProductName = "Jean";
+            product2.ProductBrand = "Pull&Bear";
+            product2.ProductModel = "Trews";
+            product2.ProductSize = 36;
+
+            Product product3 = new Product(); //Sınıfımızdaki 3.inci ürünümüzü ve özelliklerini belirlerdik.
+            product3.ProductName = "Jumper";
+            product3.ProductBrand = "Pull&Bear";
+            product3.ProductModel = "Turtleneck Jumper";
+            product3.ProductSize = 36;
+
+            Product[] products = new Product[] { product1, product2, product3 }; // Sınıfımızın değişkenlerini listeye atadık.
+
+            foreach (var product in products) //Foreach döngüsüyle listemizde yer alan değişkenleri ekrana yazdırdık.
+            {
+                Console.WriteLine(product.ProductName + ":" + product.ProductBrand + ":" + product.ProductModel + ":" + product.ProductSize);
+
+            }
+
+            Console.WriteLine("The End!");
+        }
+
+    }
+    class Product // Product isimli bir class oluşturduk. Çünkü birden fazla değişken tutmak istiyoruz. 
+    {
+        public string ProductName { get; set; } //değişkenimizin veri tipi string olduğu için string yazdık.
+        public string ProductBrand { get; set; }
+        public string ProductModel { get; set; }
+        public int ProductSize { get; set; } //değişkenimizin veri tipi int olduğu için string yazdık.
+
+    }
+
+}
---
+++ Ders2-C#/Donguler2/Donguler-son/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("--------------------------------------------------------");
+        Console.WriteLine("For Döngüsü:"); //Değişkenlerimizi tanımladık.
+
+        string product1 = "Kadın";
+        string product2 = "Erkek";
+        string product3 = "Çoçuk";
+        string product4 = "İndirim";
+        string product5 = "Koleksiyon";
+
+        //Console.WriteLine (product1); // İlk önce tüm değişkenlerimizi tek tek ekrana yazdırdık.
+        //Console.WriteLine (product2);
+        //Console.WriteLine (product3);
+        //Console.WriteLine (product4);
+        //Console.WriteLine (product5);
+
+        string[] products = new string[] { product1, product2, product3, product4, product5 }; // Değişkenlerimizi listeye atadık. 
+
+
+        for (int i = 0; i < products.Length; i += 1) // For döngüsüyle değişkenlerimizi ekrana yazdırdık. Listemizin sıfırıncı elemanından liste uzunluğuna kadar olan elemanları indexi birer arttırarak yazdırdık.
+        {
+            Console.WriteLine(products[i]);
+
+        }
+
+        Console.WriteLine("--------------------------------------------------------");
+
+        Console.WriteLine("Foreach Döngüsü:"); 
+
+        //Foreach döngüsü, listenin elemanları kadar yazdırmamızı sağlar.Eleman sayısını bilmediğimiz durumlarda çok kullanışlı bir döngü."
+
+        foreach (string product in products)
+        {
+            Console.WriteLine(product);
+        }
+
+        Console.WriteLine("--------------------------------------------------------");
+        Console.WriteLine("While Döngüsü:"); 
+
+        //string brand1 = "Nike";
+        //string brand2 = "Adidas";
+        //string brand3 = "Puma";
+        //string brand4 = "Reebok";
+        //string brand5 = "Skecher";
+
+        //string[] brands = new string[] { brand1, brand2, brand3, brand4, brand5 };
+
+        int k = 0;
+        while (k < products.Length) //While döngüsü de for, foreach işlevi görür. k değişkenimize sıfırı atadık ve listemizin uzunluğundan küçük olma koşulu sağlandığı durumda, içerisinde yer alan kod çalışsın.
+        {
+            Console.WriteLine(products[k]);
+            k++;
+
+        }
+        Console.WriteLine("--------------------------------------------------------"); //Güzel gözüksün istedim :)
+        Console.WriteLine("The End!");
+
+
+    }
+}
